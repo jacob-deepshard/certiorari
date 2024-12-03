663ee66cd6a610f3354142c7e03f59fc64ffbcf2
Jacob Valdez <jacob@deepshard.org>
2024-12-03 15:40:54 -0800

    tools with stop method

--- certiorari/utils.py
+++ certiorari/utils.py
@@ -40,6 +40,21 @@ class Tool(BaseModel):
     description: str
     start: Callable
     stop: Optional[Callable] = None
+    
+    def __call__(self, *args, **kwargs):
+        return self.start(*args, **kwargs)
+    
+    def on_stop(self, fn: Callable):
+        self.stop = fn
+        
+    def register_with_truffle(self):
+        if self.start and self.stop:
+            truffle.register_tool(self.start, name=f'start_{self.name}')
+            truffle.register_tool(self.stop, name=f'stop_{self.name}')
+        elif self.start:
+            truffle.register_tool(self.start, name=self.name)
+        else:
+            raise ValueError(f'Tool {self.name} must have a start method')
 
 class App(BaseModel):
 
@@ -63,81 +78,75 @@ class App(BaseModel):
     @metadata.setter
     def metadata(self, metadata: truffle.AppMetadata):
         self.__dict__.update(metadata.model_dump())
-
-    # def tool(self, tool: Callable):
-    #     # TODO: we can currently do this
-    #     # @app.tool
-    #     # def my_tool(arg1, arg2):
-    #     #     ...
-    #     # but we want to make this the default start method and then make it so that you can also decorate a stop method
-    #     # @app.tool
-    #     # def my_tool(arg1, arg2):
-    #     #     ...
-    #     # my_tool()
-
-    #     @functools.wraps(tool)
-    #     def wrapper(*args, **kwargs):
-    #         return tool(*args, **kwargs)
-
-    #     truffle.register_tool(wrapper)
-    #     self.tools.append(wrapper)
-    #     assert tool.__name__ is not None
-    #     assert not hasattr(self, tool.__name__)
-    #     setattr(self, tool.__name__, wrapper)
     
     def tool(self, name: str=None, description: str=None, start_fn: Callable=None, stop_fn: Callable=None):
         '''
-        Decorate a function to be a tool.
+        Make a function into a tool. Use like a decorator or a function call.
         
-        @app.tool
-        def my_tool(arg1, arg2):
-            ...
-        my_tool() # works like my_tool would normally
+        Example as a decorator:
+        >>> app = App(name="test", fullname="Test App", description="Test app", goal="Testing")
+        >>> @app.tool
+        ... def my_tool(arg1, arg2):
+        ...     return f"{arg1} {arg2}"
+        >>> my_tool(1, 2)  # works like my_tool would normally as a function
+        '1 2'
+        
+        Example showing a tool with start and stop functions:
+        >>> from concurrent.futures import ThreadPoolExecutor
+        >>> import time
+        >>> thread_pool = ThreadPoolExecutor(max_workers=4)
+        >>> def heavy_computation(x): 
+        ...     time.sleep(1)  # Simulate delay
+        ...     result = x * 2
+        ...     print(f"Computed {x} * 2 = {result}")
+        ...     return result
+        
+        >>> @app.tool
+        ... def process_data(data_array):
+        ...     """Process array of data in background thread pool"""
+        ...     futures = [thread_pool.submit(heavy_computation, x) for x in data_array]
+        ...     return [f.result() for f in futures]
+        >>> process_data([1, 2, 3, 4])
+        Computed 1 * 2 = 2
+        Computed 2 * 2 = 4
+        Computed 3 * 2 = 6
+        Computed 4 * 2 = 8
+        [2, 4, 6, 8]
+        
+        >>> @process_data.on_stop
+        ... def cleanup_threads():
+        ...     """Clean up thread pool when stopping"""
+        ...     thread_pool.shutdown(wait=True)
+        >>> thread_pool.shutdown()  # cleanup for doctest
+        
+        >>> tool = app.tool(name="process_data", description="Process array of data in background thread pool")
+        >>> tool.start(list(range(10)))
+        >>> time.sleep(3.5)
+        >>> tool.stop()
+        Computed 0 * 2 = 0
+        Computed 1 * 2 = 2
+        Computed 2 * 2 = 4
+        
+        Example as a function call:
+        >>> tool = app.tool(start_fn=process_data, stop_fn=cleanup_threads)
+        >>> tool.start(list(range(10)))
+        >>> time.sleep(3.5)
+        >>> tool.stop()
         '''
         def decorator(_start_fn: Callable):
-            name = name or _start_fn.__name__
-            description = description or _start_fn.__doc__ or str(inspect.signature(_start_fn))
-            tool = Tool(name=name, description=description, start=_start_fn)
+            _name = name or _start_fn.__name__
+            _description = description or _start_fn.__doc__ or str(inspect.signature(_start_fn))
+            tool = Tool(name=_name, description=_description, start=_start_fn, stop=stop_fn)
             self.tools.append(tool)
-            
-            @functools.wraps(_start_fn)
-            def wrapper(*args, **kwargs):
-                return _start_fn(*args, **kwargs)
-            
-            @functools.wraps(tool.start)
-            def _start_fn(*args, **kwargs):
-                return tool.start(*args, **kwargs)
-            
-            wrapper.start = _start_fn
-            
-            def on_stop(_stop_fn: Callable):
-                
-                @functools.wraps(_stop_fn)
-                def stop(*args, **kwargs):
-                    return _stop_fn(*args, **kwargs)
-                wrapper.stop = stop
-            
-            if stop_fn is not None:
-                wrapper.on_stop(_stop_fn=stop_fn)
-            
-            return wrapper
-    
+            return tool
+        
         if start_fn is not None:
-            return decorator(_start_fn=start_fn)
+            return decorator(start_fn)
         return decorator
 
-    def _register_tools(self):
-        for tool in self.tools:
-            if tool.start and tool.stop:
-                truffle.register_tool(tool.start, name=f'start_{tool.name}')
-                truffle.register_tool(tool.stop, name=f'stop_{tool.name}')
-            elif tool.start:
-                truffle.register_tool(tool.start, name=tool.name)
-            else:
-                raise ValueError(f'Tool {tool.name} must have a start method')
-
     def start(self):
-        self._register_tools()
+        for tool in self.tools:
+            tool.register_with_truffle()
         
         # the naming is outdated but this is just starting a grpc server that exposes the endpoints we decorated with @app.tool
         return truffle.build_app_from_class(self)
